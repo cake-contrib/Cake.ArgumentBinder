17ed8b4a925267f249f0deaf1ba5cd5a7e598989
Pascal Berger <pascal.berger@gmail.com>
2020-10-25 23:41:24 +0100

    Add attribute to have aliases listed on cakebuild.net

---
+++ Cake.ArgumentBinder/ArgumentBinderAliases.cs
@@ -0,0 +1,15 @@
+﻿//
+// Copyright Seth Hendrick 2019.
+// Distributed under the MIT License.
+// (See accompanying file LICENSE in the root of the repository).
+//
+
+using Cake.Core.Annotations;
+
+namespace Cake.ArgumentBinder
+{
+    [CakeAliasCategory("Arguments")]
+    public static partial class ArgumentBinderAliases
+    {
+    }
+}

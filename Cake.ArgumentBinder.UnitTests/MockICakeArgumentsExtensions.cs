3bd1af8953caf28cba0112c12e768ae5d34aea79
Seth Hendrick <seth@shendrick.net>
2021-02-07 21:48:22 -0500

    Forgot to add a file :(

---
+++ Cake.ArgumentBinder.UnitTests/MockICakeArgumentsExtensions.cs
@@ -0,0 +1,29 @@
+//
+// Copyright Seth Hendrick 2019.
+// Distributed under the MIT License.
+// (See accompanying file LICENSE in the root of the repository).
+//
+
+using System;
+using System.Collections.Generic;
+using Cake.Core;
+using Moq;
+
+namespace Cake.ArgumentBinder.UnitTests
+{
+    public static class MockICakeArgumentsExtensions
+    {
+        // ---------------- Functions ----------------
+
+        public static void SetupGetArgumentSingle(
+            this Mock<ICakeArguments> args,
+            string argName,
+            string returnValue
+        )
+        {
+            args.Setup(
+                a => a.GetArguments( argName )
+            ).Returns( new List<string>{ returnValue } );
+        }
+    }
+}

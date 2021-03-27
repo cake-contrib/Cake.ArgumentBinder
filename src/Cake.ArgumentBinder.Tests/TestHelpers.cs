ad3942b1a7da6c35b7f3d2e6fd473135fa093e52
Seth Hendrick <seth@shendrick.net>
2021-03-27 18:04:55 -0400

    Began adding unit tests to show the description of the argument.

---
+++ src/Cake.ArgumentBinder.Tests/TestHelpers.cs
@@ -0,0 +1,56 @@
+﻿//
+// Copyright Seth Hendrick 2019-2021.
+// Distributed under the MIT License.
+// (See accompanying file LICENSE in the root of the repository).
+//
+
+using System;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using NUnit.Framework;
+
+namespace Cake.ArgumentBinder.Tests
+{
+    public static class TestHelpers
+    {
+        public static void EnsureLineExistsFromMultiLineString( string expectedLine, string multiLineString )
+        {
+            Regex regex = new Regex( $@"\s+{Regex.Escape( expectedLine )}" );
+
+            EnsureLineExistsFromMultiLineString( regex, multiLineString );
+        }
+
+        public static void EnsureLineExistsFromMultiLineString( Regex expectedLine, string multiLineString )
+        {
+            int lineCount = 0;
+            StringBuilder foundLines = new StringBuilder();
+            using( StringReader reader = new StringReader( multiLineString ) )
+            {
+                string line = reader.ReadLine();
+                while( line != null )
+                {
+                    if( expectedLine.IsMatch( line ) )
+                    {
+                        ++lineCount;
+                        foundLines.AppendLine( "-" + line );
+                    }
+                    line = reader.ReadLine();
+                }
+            }
+
+            if( lineCount == 0 )
+            {
+                Assert.Fail(
+                    $"Found no matching lines for {expectedLine}.{Environment.NewLine}All lines: {Environment.NewLine}{multiLineString}"
+                );
+            }
+            else if( lineCount != 1 )
+            {
+                Assert.Fail(
+                    $"Found too many lines:{Environment.NewLine}{foundLines}"
+                );
+            }
+        }
+    }
+}
---
+++ src/Cake.ArgumentBinder.Tests/UnitTests/IntegerArgumentAttributeShowDescriptionTests.cs
@@ -0,0 +1,80 @@
+﻿//
+// Copyright Seth Hendrick 2019-2021.
+// Distributed under the MIT License.
+// (See accompanying file LICENSE in the root of the repository).
+//
+
+using NUnit.Framework;
+
+namespace Cake.ArgumentBinder.Tests.UnitTests
+{
+    [TestFixture]
+    public class IntegerArgumentAttributeShowDescriptionTests
+    {
+        // ---------------- Fields ----------------
+
+        private const string taskDescription = "Task Description";
+        private const string argumentName = "int_argument";
+        private const string description = "Some Description";
+        private const int defaultValue = 1;
+        private const int minValue = -1;
+        private const int maxValue = 3;
+
+        // ---------------- Tests ----------------
+
+        [Test]
+        public void IntegerArgumentNotHiddenTest()
+        {
+            // Act
+            string actualDescription = ArgumentBinder.GetDescription<IntegerArgumentNotHidden>( taskDescription );
+
+            // Check
+
+            // Should include the argument name.
+            TestHelpers.EnsureLineExistsFromMultiLineString(
+                $"--{argumentName}",
+                actualDescription
+            );
+
+            // Should include the description.
+            TestHelpers.EnsureLineExistsFromMultiLineString(
+                description,
+                actualDescription
+            );
+
+            // Should include the type.
+            TestHelpers.EnsureLineExistsFromMultiLineString(
+                $"Type: {typeof( int ).Name}",
+                actualDescription
+            );
+
+            // Should include the min value.
+            TestHelpers.EnsureLineExistsFromMultiLineString(
+                $"Minimum Value: {minValue}",
+                actualDescription
+            );
+
+            // Should include the max value.
+            TestHelpers.EnsureLineExistsFromMultiLineString(
+                $"Maximum Value: {maxValue}",
+                actualDescription
+            );
+        }
+
+        // ---------------- Helper Classes ----------------
+
+        public class IntegerArgumentNotHidden
+        {
+            [IntegerArgument(
+                argumentName,
+                Description = description,
+                DefaultValue = defaultValue,
+                Min = minValue,
+                Max = maxValue,
+                HasSecretValue = false,
+                Required = true
+            )]
+            public int IntegerArgument { get; set; }
+        }
+    }
+}
